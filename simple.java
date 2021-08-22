bc114581b43ccbd458a3a2a3f12fcf4afce0dd75
Thirulok-Ranganathan <73477145+Thirulok-Ranganathan@users.noreply.github.com>
2021-08-22 14:48:05 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,5 +1,6 @@
+//Just a start in all programs
 class simple{
 	public static void main(String args[]){
-	System.out.print("Hello World");
+	System.out.print("Hello World");//prints hello world
 	}
 }
