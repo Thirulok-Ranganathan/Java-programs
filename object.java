9cd6222bf3f22586d9b111588261775fa1005d9a
Thirulok-Ranganathan <73477145+Thirulok-Ranganathan@users.noreply.github.com>
2021-08-22 13:26:40 +0530

    Add files via upload

---
+++ object.java
@@ -0,0 +1,10 @@
+//Simple Example of Object and Class without input
+class object{
+	int id;
+	String name;
+	public static void main(String args[]){
+		object s1=new object();
+		System.out.println(s1.id);
+		System.out.print(s1.name);
+	}
+}
