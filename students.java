f9a3e964dc6e2d7a5854271e14350483d10300b5
Thirulok-Ranganathan <73477145+Thirulok-Ranganathan@users.noreply.github.com>
2021-08-22 14:57:35 +0530

    Update students.java

--- students.java
+++ students.java
@@ -10,7 +10,7 @@ class students{
 	public static void main(String args[]){
 		students s1=new students();
 		students s2=new students();
-		s1.insertRecord(101,"Drakeus");	  //adding student values in record		
+		s1.insertRecord(101,"Drakeus");	  //insert values in record		
 		s2.insertRecord(102,"Joe");
 		s1.displayInformation();
 		s2.displayInformation();
